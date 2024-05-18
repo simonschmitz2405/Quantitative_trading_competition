0ec469265f806236e2c587c392fc38b164c973de
Simon Schmitz <simonschmitz@outlook.de>
2024-05-18 13:59:44 +0200

    Change

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ import pandas as pd
 def main():
     """The main function of the trading bot."""
     # Initialize the DataReader class and get the stock data
-    # data_reader = DataReader("2024-01-01")
-    # data_reader.run()
-    portfolio = Portfolio(streakLength=6, thresholdType="returnRaw")
-    portfolio = Portfolio(streakLength=7, thresholdType="returnRaw")
-    portfolio = Portfolio(streakLength=8, thresholdType="returnRaw")
-
+    data_reader = DataReader("2024-01-01")
+    data_reader.run()
+    portfolio = Portfolio(streakLength=5, thresholdType="returnRaw")
     portfolio.visualize_portfolio()
 
     try:
