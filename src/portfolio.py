36f7dc330c2d03556d673ec9b41b3591f13eef4a
Simon Schmitz <simonschmitz@outlook.de>
2024-05-28 21:49:43 +0200

    Add feature value weighted

--- src/portfolio.py
+++ src/portfolio.py
@@ -9,10 +9,14 @@ import logging
 
 
 class Portfolio:
-    def __init__(self, streakLength=4, thresholdType="returnRaw") -> None:
+    def __init__(self, streakLength, thresholdType, valueWeighted) -> None:
         """Initialize the Portfolio class."""
         # Define the logging
-        logging.basicConfig(level=logging.INFO, filename=r"C:\Users\simon\OneDrive\Dokumente\[1] Uni\[1] Master\2. Semester Sommersemester 2024\Quantitative_trading_competition\Code\Quantitative_trading_competition\src\logfile.log", filemode="w")
+        logging.basicConfig(
+            level=logging.INFO,
+            filename=r"C:\Users\simon\OneDrive\Dokumente\[1] Uni\[1] Master\2. Semester Sommersemester 2024\Quantitative_trading_competition\Code\Quantitative_trading_competition\src\logfile.log",
+            filemode="w",
+        )
 
         # Define the paths
         self._pathSp500StockData = r"C:\Users\simon\OneDrive\Dokumente\[1] Uni\[1] Master\2. Semester Sommersemester 2024\Quantitative_trading_competition\Code\Quantitative_trading_competition\data\sp500_stock_data.csv"
@@ -26,7 +30,7 @@ class Portfolio:
         self.streakLength = streakLength
         self.thresholdingType = thresholdType
         self.holdingPeriod = None  # TODO: Implement
-        self.weighting = None  # TODO: Implement
+        self.valueWeighted = valueWeighted
         self.longShort = None  # TODO: Implement
 
         # Initialize the dataFrames which are used later
@@ -58,7 +62,7 @@ class Portfolio:
         print(f"Streak length: {self.streakLength}")
         print(f"Thresholding type: {self.thresholdingType}")
         print(f"Holding period: {self.holdingPeriod}")
-        print(f"Weighting: {self.weighting}")
+        print(f"Weighting: {self.valueWeighted}")
         print(f"Long/Short: {self.longShort} \n")
 
     def _choose_thresholding_type(self, thresholdType) -> int:
@@ -107,21 +111,18 @@ class Portfolio:
     #             previous_day
     #         ]
     #     return pd.DataFrame(previous_returns)
-    
 
-    def _get_previous_returns(self, formation, streak_length=5) -> pd.DataFrame:
+    def _get_previous_returns(self, formation) -> pd.DataFrame:
         """Get the previous returns for the given streak length."""
         previous_returns = {}
         for i in range(1, self.streakLength + 1):
             dates_list = self.stockData["DATE"].tolist()
             index = dates_list.index(formation)
 
-            previous_returns[f"ret_{i}"] = self.returns.loc[
-                dates_list[index - i]
-            ]
+            previous_returns[f"ret_{i}"] = self.returns.loc[dates_list[index - i]]
         return pd.DataFrame(previous_returns)
 
-    def _get_previous_market_excess_returns(self, formation, streak_length=5) -> pd.DataFrame:
+    def _get_previous_market_excess_returns(self, formation) -> pd.DataFrame:
         """Get the previous market excess returns for the given streak length."""
         previous_market_excess_returns = {}
         for i in range(1, self.streakLength + 1):
@@ -138,12 +139,10 @@ class Portfolio:
         """Calculate if there is a streak raw return."""
         return 1 if (x > 0).all() or (x < 0).all() else 0
 
-    def _calculate_loser_winner_streaks(self, formation, streak_length=5) -> tuple:
+    def _calculate_loser_winner_streaks(self, returnDay) -> tuple:
         """Calculate the loser and winner streaks for the given formation date."""
-        returnStreakRaw = pd.DataFrame(self._get_previous_returns(formation, streak_length))
-        returnStreakMarket = pd.DataFrame(
-            self._get_previous_market_excess_returns(formation, streak_length)
-        )
+        returnStreakRaw = pd.DataFrame(self._get_previous_returns(returnDay))
+        returnStreakMarket = pd.DataFrame(self._get_previous_market_excess_returns(returnDay))
         streaks = pd.DataFrame(index=returnStreakRaw.index)
         streaks["Streak_Raw_Return"] = returnStreakRaw.apply(
             lambda x: self._calculate_streak(x), axis=1
@@ -154,21 +153,87 @@ class Portfolio:
         thresholdType = self._choose_thresholding_type(self.thresholdingType)
         losersRawReturn = streaks[streaks[thresholdType] < 0].index
         winnersRawReturn = streaks[streaks[thresholdType] > 0].index
-        print("Amount of Stock for this day that are losers", len(losersRawReturn))
+        # print("Amount of Stock for this day that are losers", len(losersRawReturn))
+        # print('Date', returnDay)
+        # print("LoserIndex", losersRawReturn)
+        losRetRawReturn = None
+        winRetRawReturn = None
 
-        # TODO: Calculate the weighted returns for the losers and winners
-        # Calculate the average raw return for losers and winners (Equal weighted)
-        losRetRawReturn = self.returns.loc[
-            formation, self.returns.columns.isin(losersRawReturn)
-        ].mean()
-        winRetRawReturn = self.returns.loc[
-            formation, self.returns.columns.isin(winnersRawReturn)
-        ].mean()
+
+        # Check if the value weighted return should be calculated
+        if self.valueWeighted is True:
+            weightsLoser = []
+            weightsWinner = []
+
+            # Calculate the Returns for the winners and losers
+            if not losersRawReturn.tolist():
+                losRetRawReturn = 0
+            else:
+                # Get the returns for the losers and winners
+                losRetRawReturn = self.returns.loc[
+                    returnDay, self.returns.columns.isin(losersRawReturn)
+                ]
+            if not winnersRawReturn.tolist():
+                winRetRawReturn = 0
+            else:    
+                winRetRawReturn = self.returns.loc[
+                    returnDay, self.returns.columns.isin(winnersRawReturn)
+                ]
+            
+           # Calculate the weights for the winners and losers
+            for ticker in losersRawReturn.tolist():
+                weightsLoser.append(
+                    self.stockData.loc[
+                        (self.stockData["TICKER"] == ticker)
+                        & (self.stockData["DATE"] == returnDay),
+                        "MARKET_CAP",
+                    ].values[0]
+                )
+
+            for ticker in winnersRawReturn.tolist():
+
+                weightsWinner.append(
+                    self.stockData.loc[
+                        (self.stockData["TICKER"] == ticker)
+                        & (self.stockData["DATE"] == returnDay),
+                        "MARKET_CAP",
+                    ].values[0]
+                )
+
+            # Calculate the relative weights
+            weightsLoser = weightsLoser / np.sum(weightsLoser)
+            weightsWinner = weightsWinner / np.sum(weightsWinner)
+
+            if not losersRawReturn.tolist():
+                weightsLoser = 0
+
+            if not winnersRawReturn.tolist():
+                weightsWinner = 0
+
+            # Calculate the value weighted return
+            losRetRawReturn = np.dot(losRetRawReturn, weightsLoser)
+            winRetRawReturn = np.dot(winRetRawReturn, weightsWinner)
+
+        else:
+
+            if not losersRawReturn.tolist():
+                losRetRawReturn = 0
+            else:
+                # Get the returns for the losers and winners
+                losRetRawReturn = self.returns.loc[
+                    returnDay, self.returns.columns.isin(losersRawReturn)
+                ].mean()
+            if not winnersRawReturn.tolist():
+                winRetRawReturn = 0
+            else:    
+                winRetRawReturn = self.returns.loc[
+                    returnDay, self.returns.columns.isin(winnersRawReturn)
+                ].mean()
 
         # return losret, winret
         return losRetRawReturn, winRetRawReturn
 
-    def _calculate_long_short_portfolio(self, streakLength=5) -> tuple:
+    def _calculate_long_short_portfolio(self) -> tuple:
         """Calculate the daily returns for given streak length."""
         returnsLong = []
         returnsShort = []
@@ -176,33 +241,25 @@ class Portfolio:
         datesShort = []
 
         for date in self.returns.index[8:]:
-            print(date)
-            long_return, short_return = self._calculate_loser_winner_streaks(
-                pd.Timestamp(date), streakLength
-            )
-            print(long_return, short_return)
-
-            # Füge die Long-Rendite und das Datum hinzu, wenn ein Long-Rendite-Wert berechnet wurde
-            if pd.notna(long_return):
-                returnsLong.append(long_return)
-                datesLong.append(pd.Timestamp(date).date())
-
-            # Füge die Short-Rendite und das Datum hinzu, wenn ein Short-Rendite-Wert berechnet wurde
-            if pd.notna(short_return):
-                returnsShort.append(short_return)
-                datesShort.append(pd.Timestamp(date).date())
+            # print(date)
+            long_return, short_return = self._calculate_loser_winner_streaks(pd.Timestamp(date))
+            
+            returnsLong.append(long_return)
+            datesLong.append(pd.Timestamp(date).date())
+            returnsShort.append(short_return)
+            datesShort.append(pd.Timestamp(date).date())
 
         return returnsLong, returnsShort, datesLong, datesShort
 
     def _plot_data(self) -> None:
         """Plot the daily returns for the long and short portfolios."""
-        logging.info('Daily Returns Long', self.dailyReturnsLong)
-        logging.info('Daily Returns Short', self.dailyReturnsShort)
+        logging.info("Daily Returns Long", self.dailyReturnsLong)
+        logging.info("Daily Returns Short", self.dailyReturnsShort)
 
     def _calculate_annualized_return(self) -> None:
         """Calculate the annualized return."""
-        cleanReturnsLong = [x for x in self.dailyReturnsLong if str(x) != "nan"]
-        cleanReturnsShort = [x for x in self.dailyReturnsShort if str(x) != "nan"]
+        # cleanReturnsLong = [x for x in self.dailyReturnsLong if str(x) != "nan"]
+        # cleanReturnsShort = [x for x in self.dailyReturnsShort if str(x) != "nan"]
 
         longData = pd.DataFrame({"Date": self.datesLong, "Return": self.dailyReturnsLong})
 
@@ -213,8 +270,8 @@ class Portfolio:
         shortData["Date"] = pd.to_datetime(shortData["Date"])
         shortData.set_index("Date", inplace=True)
 
-        dailyReturnLong = np.array(cleanReturnsLong).mean()
-        dailyReturnShort = np.array(cleanReturnsShort).mean()
+        dailyReturnLong = np.array(self.dailyReturnsLong).mean()
+        # dailyReturnShort = np.array(self.dailyReturnsShort).mean()
 
         monthly_Return_Long = longData["Return"].resample("M").apply(lambda x: (1 + x).prod() - 1)
         monthly_Return_Short = shortData["Return"].resample("M").apply(lambda x: (1 + x).prod() - 1)
@@ -227,7 +284,7 @@ class Portfolio:
         aver_annual_Return_Long = annual_Return_Long.mean()
         aver_annual_Return_Short = annual_Return_Short.mean()
 
-        total_Return_long = np.prod([(1 + r) for r in cleanReturnsLong]) - 1
+        total_Return_long = np.prod([(1 + r) for r in self.dailyReturnsLong]) - 1
 
         print("Average daily long return ", dailyReturnLong)
         print("Average monthly long return ", aver_Monthly_Return_Long)
@@ -265,24 +322,24 @@ class Portfolio:
 
     def visualize_portfolio(self) -> None:
         """Visualize the portfolio."""
-        cleanReturnsLong = [x for x in self.dailyReturnsLong if str(x) != "nan"]
-        cleanReturnsShort = [x for x in self.dailyReturnsShort if str(x) != "nan"]
+        # cleanReturnsLong = [x for x in self.dailyReturnsLong if str(x) != "nan"]
+        # cleanReturnsShort = [x for x in self.dailyReturnsShort if str(x) != "nan"]
 
         initialBudget = 1
         budgetLong = [initialBudget]
         budgetMarket = [initialBudget]
-        budgetShort = [initialBudget]
+        # budgetShort = [initialBudget]
 
         min_length = min(
-            len(cleanReturnsLong), len(cleanReturnsShort), len(self.marketReturns["SP500_Returns"])
+            len(self.dailyReturnsLong), len(self.dailyReturnsShort), len(self.marketReturns["SP500_Returns"])
         )
 
         for i in range(1, min_length):
-            budgetLong.append(budgetLong[i - 1] * (1 + cleanReturnsLong[i]))
-            budgetShort.append(budgetShort[i - 1] * (1 + self.dailyReturnsShort[i]))
+            budgetLong.append(budgetLong[i - 1] * (1 + self.dailyReturnsLong[i]))
+            # budgetShort.append(budgetShort[i - 1] * (1 + self.dailyReturnsShort[i]))
             budgetMarket.append(budgetMarket[i - 1] * (1 + self.marketReturns["SP500_Returns"][i]))
         plt.plot(budgetLong, label="Long Portfolio")
         plt.plot(budgetMarket, label="Market Portfolio")
-        plt.plot(budgetShort, label="Short Portfolio")
+        # plt.plot(budgetShort, label="Short Portfolio")
         plt.legend()
         plt.show()
